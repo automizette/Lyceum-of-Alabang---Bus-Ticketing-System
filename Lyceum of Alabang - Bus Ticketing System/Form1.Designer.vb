524ca3e5cc3b296d97da01bd18baedc6bd54330c
automie009 <gamasan.ahron009@hotmail.com>
2023-05-01 09:01:46 +0800

    Add project files.

---
+++ Lyceum of Alabang - Bus Ticketing System/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnAuthenticate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Raleway ExtraBold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(297, 152)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Font = New System.Drawing.Font("Raleway SemiBold", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtUsername.Location = New System.Drawing.Point(300, 173)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(228, 23)
+        Me.TxtUsername.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Raleway ExtraBold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(297, 228)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 18)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Font = New System.Drawing.Font("Raleway SemiBold", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtPassword.Location = New System.Drawing.Point(300, 249)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(228, 23)
+        Me.TxtPassword.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Raleway Black", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label3.Location = New System.Drawing.Point(378, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 22)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "LOGIN"
+        '
+        'BtnAuthenticate
+        '
+        Me.BtnAuthenticate.BackColor = System.Drawing.Color.LightCyan
+        Me.BtnAuthenticate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnAuthenticate.Font = New System.Drawing.Font("Raleway ExtraBold", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAuthenticate.Location = New System.Drawing.Point(361, 335)
+        Me.BtnAuthenticate.Name = "BtnAuthenticate"
+        Me.BtnAuthenticate.Size = New System.Drawing.Size(109, 36)
+        Me.BtnAuthenticate.TabIndex = 5
+        Me.BtnAuthenticate.Text = "Login"
+        Me.BtnAuthenticate.UseVisualStyleBackColor = False
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(830, 456)
+        Me.Controls.Add(Me.BtnAuthenticate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lyceum of Alabang - Bus Ticketing System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtUsername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BtnAuthenticate As Button
+End Class
